9699bab711dee9d8d8c2c5eab9bd7eb182b9b384
raparisonbryan <raparisonbryan@yahoo.fr>
2025-08-10 13:38:10 +0300

    fix: railway whitelist

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const corsOptions = {
   origin: [
     "https://app-advisor-frontend-production.up.railway.app",
     "https://app-advisor-llm-production.up.railway.app",
-    "https://backend-dev-developpment.up.railway.app",
+    "https://frontend-dev-developpment.up.railway.app",
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost:5005",
